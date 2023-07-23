cf355993e74e78fa8f70a010feeae16a0febc2d5
Shagun <jaiswalshagun79@gmail.com>
2023-07-23 23:37:27 +0530

    added functionalities

--- music.js
+++ music.js
@@ -1,6 +1,37 @@
-let progress = document.getElementById('progress');
-let song     =   document.getElementById('song');
-let ctrlIcon  =  document.getElementById('ctrlIcon');
+let progress= document.getElementById('progress');
+let song= document.getElementById('song');
+let ctrlIcon= document.getElementById('ctrlIcon');
 
+
+song.onloadedmetadata = function(){
+    progress.max = song.duration;
+    progress.value = song.currentTime
+}
+function PlayPause(){
+    if(ctrlIcon.classList.contains("fa-play")){
+        song.play();
+        ctrlIcon.classList.remove("fa-play");
+        ctrlIcon.classList.add("fa-pause");
+      
+
+    }
+  
+    else{
+         
+        song.pause();
+        ctrlIcon.classList.add("fa-play");
+        ctrlIcon.classList.remove("fa-pause");
+
+
+    }
+
+  
+}
+ if(song.play()){
+         setInterval(()=>{
+            progress.value = song.currentTime;
+        } ,500)
+
+     }
 
    
